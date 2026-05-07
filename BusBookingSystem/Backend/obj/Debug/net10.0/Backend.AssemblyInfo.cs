ae8f6d1b45faa6549f4ca97adab995c2a69aa2e0
Raghav <raghavarora9899@gmail.com>
2026-05-07 23:49:42 +0530

    made skeleton three tier

--- BusBookingSystem/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ BusBookingSystem/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42f579db13bbd52c85a867ce95a1327966576353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8472767b178ec441470fec1fc698ce6252ac262a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
